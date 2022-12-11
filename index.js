3430c7ba7c22835b3e09fd8b836c9944a22b3a49
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-12-11 20:55:01 +0000

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 window.addEventListener('resize', () => {
     document.querySelector(".hi").style.height = `${window.innerHeight}px`
-    document.querySelector("section").style.height = `${window.innerHeight}px`
+    document.querySelector(".section1").style.height = `${window.innerHeight}px`
+    document.querySelector(".section2").style.height = `${window.innerHeight}px`
     document.querySelector(".hum").style.height = `${window.innerHeight}px`
   });
